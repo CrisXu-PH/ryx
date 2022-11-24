b225111246531eaf70b49fe0c0c452831657b352
Christina Xu <cxu@wesleyan.edu>
2022-11-24 11:54:50 -0500

    revise print function, complete summary

--- R/ryxFunction.R
+++ R/ryxFunction.R
@@ -21,15 +21,65 @@ ryx <- function(data, y, x){
   return(results)
 }
 
+library(MASS)
+x <- ryx(Boston, y="medv")
+
 print.ryx <- function(x){
+  if(!inherits(x, "ryx")) stop("Must be class 'ryx'")
+
   library(DT)
   cat("Correlations of ",x$y," with \n")
-  df <- x$df
-  row.names(df) <- NULL
+
+  varNames <- x$df$variable
+  rVal <- format(round(x$df$r,digits = 3), nsmall = 3)
+  pVal <- ifelse(x$df$p < 2e-16, "< 2e-16", formatC(x$df$p, format = "e", digits = 2))
+  sigif <- x$df$sigif
+
+  df <- as.data.frame(cbind(varNames, rVal, pVal, sigif))
+  df <- df[order(abs(as.numeric(df$rVal)), decreasing = TRUE),]
+  colnames(df) <- c("variable", "r","p","sigif")
+
   df
 }
 
 # print(x)
 
-plot.ryx <- function(x){
+summary.ryx <- function(x){
+  if(!inherits(x, "ryx")) stop("Must be class 'ryx'")
+
+  library(stringr)
+  x_vars <- ""
+
+  for (name in x$x){
+    x_vars<- str_trim(paste(x_vars, name, sep = " "))
+  }
+
+  strOutput <- cat(
+    "Correlating ",
+    x$y,
+    " with ",
+    x_vars,
+    "\nThe median absolute correlation was ",
+    round(median(abs(x$df$r)), digits = 3),
+    " with a range from ",
+    round(min(x$df$r),digits = 3),
+    " to ",
+    round(max(x$df$r),digits = 3),
+    "\n",
+    length(subset(x$df, p < 0.05)$p),
+    " out of ",
+    length(x$x),
+    " variables were significant at the p < 0.05 level."
+  )
+
 }
+
+# summary(x)
+
+plot.ryx <- function(x){
+  if(!inherits(x, "rys")) stop("Must be class 'ryx'")
+
+  plot(x$df)
+}
+
+plot(x)
